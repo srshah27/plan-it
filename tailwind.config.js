985d1490176c69733de67cc955b83e6ff7e5d5a7
Alefiya Abbas <alefiyar02@gmail.com>
2022-10-10 00:38:14 +0530

    UI changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       'custom-yellow': "#F9BC60",
       'custom-blue': "#5271FF",
       'custom-white': "#F5F5F5",
-      'custom-gray': "#F0F0F0",
+      'custom-gray': "#E0E0E0",
     }
     },
     fontFamily:{
