1daf6b60ace869b99744bdfd798dd902dfa5370a
Snehil Shah <snehilshah27@gmail.com>
2022-10-13 03:51:03 +0530

    many

--- components/ChannelBlock.js
+++ components/ChannelBlock.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 import { FaChevronDown, FaChevronRight, FaPlus } from 'react-icons/fa';
+import Link from 'next/Link'
 
 const today = ['Task 1', 'Task 2'];
 const tomorrow = ['Task 3', 'Presentation'];
-const  ChannelBar = () => {
+const month = new Date().toLocaleString("en-US", { month: "long" })
+const date = new Date().toLocaleString("en-US", { day: "2-digit" })
+
+const ChannelBar = () => {
     return (
         <div className='channel-bar shadow-lg'>
-            <CurDate />
-            < Divider />
-            <div className='channel-container'>
-                <Dropdown header=" Today's Tasks" selections={today} />
-                <Dropdown header="Tomorrow's Tasks" selections={tomorrow} />
-            </div>
+            {/* <CurDate /> */}
+            <Dropdown header={month + ', ' + date} selections={today} />
         </div>
     );
 };
@@ -22,18 +22,21 @@ const Dropdown = ({ header, selections }) => {
 
     return (
         <div className='dropdown'>
-            <div onClick={() => setExpanded(!expanded)} className='dropdown-header'>
-                <ChevronIcon expanded={expanded} />
-                <h5
-                    className={expanded ? 'dropdown-header-text-selected' : 'dropdown-header-text'}
-                >
-                    {header}
-                </h5>
-                <FaPlus size='12' className='text-accent text-opacity-80 my-auto ml-auto' />
+            <div className='flex flex-col'>
+                <div onClick={() => setExpanded(!expanded)} className='dropdown-header'>
+                    <ChevronIcon expanded={expanded} />
+                    <h5
+                        className={expanded ? 'dropdown-header-text-selected' : 'dropdown-header-text'}
+                    >
+                        {header}
+                    </h5>
+                    <Link href='/tasks'><a><FaPlus size='16' className='text-accent text-opacity-80 my-auto ml-32 mr-2' /></a></Link>
+                </div>
+                <Divider />
             </div>
             {expanded &&
                 selections &&
-                selections.map((selection) => <TopicSelection selection={selection} /> )}
+                selections.map((selection) => <TopicSelection selection={selection} />)}
         </div>
     );
 };
@@ -49,15 +52,15 @@ const ChevronIcon = ({ expanded }) => {
 
 const TopicSelection = ({ selection }) => (
     <div className='dropdown-selection'>
-        <FiCheckSquare size='18' className='text-gray-400 mr-2' />   
+        <FiCheckSquare size='18' className='text-gray-400 mr-2' />
         <h5 className='dropdown-selection-text'>{selection}</h5>
     </div>
 );
 
 const CurDate = () => (
-    
+
     <div className='channel-block'>
-        <h5 className='channel-block-text'>{new Date().toLocaleString("en-US", { month: "long" })}, { new Date().toLocaleString("en-US", { day : '2-digit'}) }</h5>
+        <h5 className='channel-block-text'>{new Date().toLocaleString("en-US", { month: "long" })}, {new Date().toLocaleString("en-US", { day: '2-digit' })}</h5>
     </div>
 );
 const Divider = () => <hr className="content-hr" />;
--- pages/app/index.js
+++ pages/app/index.js
@@ -3,18 +3,22 @@ import SideBar from '../../components/SideBar'
 import ChannelBar from '../../components/ChannelBlock'
 import styles from '../../styles/Home.module.css'
 import ContentContainer from '../../components/Content'
+import TopNavigation from '../../components/TopNavigation'
 
 import { useSession } from "next-auth/react"
 
 export default function Home() {
-  
+
   // if(session)
   //   console.log(session);
   return (
-    <div className="flex flex-row">
-      <SideBar />
+    <div>
+      <div className='appview'>
+        <SideBar />
+        {/* <ContentContainer /> */}
+        <TopNavigation />
+      </div>
       <ChannelBar />
-      <ContentContainer />
     </div>
   )
 }
