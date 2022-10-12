cae07db33be598137db3ca088547cccc0af78370
Alefiya Abbas <alefiyar02@gmail.com>
2022-10-12 21:53:09 +0530

    changes made

--- components/sidebar.js
+++ components/sidebar.js
@@ -10,7 +10,7 @@ const SideBar = () => {
             <div className="fixed top-0 left-0 h-screen w-16 flex flex-col flex-start
                   bg-custom-cream shadow-lg">
 
-                <SideBarLogo icon={<Image src="/img/Planit.svg" width={75} height={60} />} />
+                <SideBarLogo icon={<Image src="/img/Planit.svg" width={90} height={72} />} />
                 <Divider />
                 <SideBarIcon icon={<BsPlus size="32" />} text ={"Add"} />
                 <SideBarIcon icon={<IoMdCheckmark size="32" />} text ={"Done"} />
