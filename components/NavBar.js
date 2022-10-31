2a7e4b337c960653efe95e5fac8cb7b967592814
Snehil Shah <snehilshah27@gmail.com>
2022-10-31 18:46:27 +0530

    added notification

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image';
 import Planit from '../public/img/Planit.svg'
+import Link from 'next/link';
+import toast, { Toaster } from 'react-hot-toast';
+
+
+
 const NavBar = () => {
     return (
         <div className={styles.topnavigation} >
@@ -10,12 +15,17 @@ const NavBar = () => {
                 height="80px"
             />
             <Title />
-            <button className={styles.signup}>Sign Up!</button>
+            <div>
+                <button onClick={notify} className={styles.signup}>Add Task!</button>
+                <Toaster />
+            </div>
         </div>
     );
 };
 
 const Title = () => <h5 className={styles.titletext}>Plan-it!</h5>;
+const notify = () => toast('Task Added');
 
 
 export default NavBar;
+
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import HomePg from '../components/HomePg'
 
 export default function index() {
+  useEffect(() => {
+    if(Notification.permission === 'granted'){
+        
+    }
+    else{
+      Notification.requestPermission().then((response) => {
+        if(response === 'granted'){
+          const notify = new Notification("Planned IT!")
+        }
+        else{
+          console.log("Notification permission denied")
+        }
+      })
+    }
+  }, [])
   return (
     // <div>index</div>
     <div className="flex-col">
